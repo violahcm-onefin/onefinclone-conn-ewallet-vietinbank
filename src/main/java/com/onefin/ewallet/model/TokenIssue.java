e6f8dfcba2800a2553e786489ea33b52bd64d91d
tahongbaothai_onefin <thaita@onefin.vn>
2021-06-09 14:32:49 +0700

    Updated

--- src/main/java/com/onefin/ewallet/model/TokenIssue.java
+++ src/main/java/com/onefin/ewallet/model/TokenIssue.java
@@ -9,7 +9,7 @@ import lombok.Data;
 public class TokenIssue {
 
 	@Size(max = 20)
-	@NotEmpty(message = "Not empty")
+	@NotEmpty(message = "Not empty cardNumber")
 	private String cardNumber;
 
 	@Size(max = 6)
@@ -17,34 +17,34 @@ public class TokenIssue {
 	private String cardIssueDate;
 
 	@Size(max = 50)
-	@NotEmpty(message = "Not empty")
+	@NotEmpty(message = "Not empty cardHolderName")
 	private String cardHolderName;
 
 	@Size(max = 30)
-	@NotEmpty(message = "Not empty")
+	@NotEmpty(message = "Not empty providerCustId")
 	private String providerCustId;
 
 	@Size(max = 30)
-	@NotEmpty(message = "Not empty")
+	@NotEmpty(message = "Not empty custPhoneNo")
 	private String custPhoneNo;
 
 	@Size(max = 30)
-	@NotEmpty(message = "Not empty")
+	@NotEmpty(message = "Not empty custIDNo")
 	private String custIDNo;
 
 	@Size(max = 16)
 	private String clientIP;
 
 	@Size(max = 14)
-	@NotEmpty(message = "Not empty")
+	@NotEmpty(message = "Not empty transTime")
 	private String transTime;
 
 	@Size(max = 12)
-	@NotEmpty(message = "Not empty")
+	@NotEmpty(message = "Not empty requestId")
 	private String requestId;
 
 	@Size(max = 15)
-	@NotEmpty(message = "Not empty")
+	@NotEmpty(message = "Not empty channel")
 	private String channel;
 
 	@Size(max = 3)
