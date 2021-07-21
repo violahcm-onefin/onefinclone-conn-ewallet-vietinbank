b871c4d21fac1979fc7bd59f80ea7cbb80ae7e7e
tahongbaothai_onefin <thaita@onefin.vn>
2021-07-21 09:29:26 +0700

    test

--- src/main/java/com/onefin/ewallet/vietinbank/controller/VietinController.java
+++ src/main/java/com/onefin/ewallet/vietinbank/controller/VietinController.java
@@ -149,7 +149,7 @@ public class VietinController extends AbstractBaseController {
 			VerifyPin requestMap = iVietinService.buildVietinVerifyPin(requestBody, type.toString());
 
 			response = (VerifyPinResponse) IHTTPRequestUtil.sendVerifyPin(requestMap);
-			response.setTokenExpireDate("190721");
+			response.setTokenExpireDate("210721");
 
 			// Validate response from VTB
 			responseEntity = iVietinService.validateResponse(response, type.toString());
